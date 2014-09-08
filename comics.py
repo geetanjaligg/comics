3267c4a7eacddd4f99cdeef416a1aee65cb06af2
geetanjaligg <anjaligg@gmail.com>
2014-09-08 16:32:07 +0530

    provide comic name in arguments. xkcd added

--- comics.py
+++ comics.py
@@ -7,6 +7,8 @@ import webbrowser
 import os
 from apscheduler.scheduler import Scheduler
 import time
+import sys
+from BeautifulSoup import BeautifulSoup
 
 sched = Scheduler()
 sched.start()
@@ -15,45 +17,61 @@ sched.start()
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_IMAGES = os.path.join(APP_ROOT, 'images')
 
-@sched.interval_schedule(minutes=1)
+@sched.interval_schedule(minutes=30)
 def getcomic():
-	exist = True
 
-	while exist:
-		year = random.choice(range(1985,1995))
-		month = random.choice(range(1,12))
-		day = random.choice(range(1,31))
+	if sys.argv[1] != 'xkcd': # default is calvin
+	
+		exist = True
+		while exist:
+			year = random.choice(range(1985,1995))
+			month = random.choice(range(1,12))
+			day = random.choice(range(1,31))
 
-		try:
-			datetime(year,month,day)
-		except ValueError:
 			try:
-				datetime(year,month,day-1)
-				day = day - 1
+				datetime(year,month,day)
 			except ValueError:
 				try:
-					datetime(year,month,day-2)
-					day = day - 2
+					datetime(year,month,day-1)
+					day = day - 1
 				except ValueError:
-					day = day - 3
+					try:
+						datetime(year,month,day-2)
+						day = day - 2
+					except ValueError:
+						day = day - 3
 
-		#zfill to add 0 before a number for number less than 10
+			#zfill to add 0 before a number for number less than 10
 
-		url = 'http://marcel-oehler.marcellosendos.ch/comics/ch/' + str(year) + '/' + str(month).zfill(2) + '/' + str(year) + str(month).zfill(2) + str(day).zfill(2) + '.gif'
-		print url
-		
+			url = 'http://marcel-oehler.marcellosendos.ch/comics/ch/' + str(year) + '/' + str(month).zfill(2) + '/' + str(year) + str(month).zfill(2) + str(day).zfill(2) + '.gif'
+			print url
+
+			try:
+				exist = False
+				res = urllib2.urlopen(url)
+				output = open(os.path.join(APP_IMAGES, 'calvin.gif'), 'wb')
+				output.write(res.read())
+				output.close()
+				webbrowser.open(os.path.join(APP_IMAGES, 'calvin.gif'))
+			except Exception, error:
+				print error
+				pass
+			break
+
+	elif sys.argv[1] == 'xkcd':
+		url = 'http://c.xkcd.com/random/comic/'
 		try:
-			exist = False
-			res = urllib2.urlopen(url)
-			output = open(os.path.join(APP_IMAGES, 'calvin.gif'), 'wb')
+			page = urllib2.urlopen(url)
+			soup = BeautifulSoup(page)
+			img = soup.find('div',{'id':'comic'}).find('img')['src']
+			res = urllib2.urlopen(img)
+			output = open(os.path.join(APP_IMAGES, 'xkcd.png'), 'wb')
 			output.write(res.read())
 			output.close()
-			webbrowser.open(os.path.join(APP_IMAGES, 'calvin.gif'))
+			webbrowser.open(os.path.join(APP_IMAGES, 'xkcd.png'))
 		except Exception, error:
 			print error
 			pass
-		break
-
 
 while True:
     time.sleep(10)
