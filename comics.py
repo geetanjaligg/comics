b50d5afdf2c96c470bbec7a1b7710d51f1dddd53
Murali Ravipudi <murali.ravipudi@gmail.com>
2018-03-21 23:47:32 +0530

    Python 3 compatibility
    Polyglot code.
    Tweak to make image display on macos

--- comics.py
+++ comics.py
@@ -1,6 +1,7 @@
 # Thanks to Marcel Oehler for Comic strips
 
-import urllib2
+from __future__ import print_function
+import urllib.request, urllib.error, urllib.parse
 from datetime import datetime
 import random
 import webbrowser
@@ -17,16 +18,24 @@ sched.start()
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_IMAGES = os.path.join(APP_ROOT, 'images')
 
+PLATFORM=sys.platform
+AVAILABLE_COMICS = ['calvin', 'xkcd', 'explosm']
+
 @sched.scheduled_job(trigger='interval', minutes=30)
 def getcomic():
 
-	if len(sys.argv) < 2 or sys.argv[1] == 'calvin': # default is calvin
-		# print 'inside'
+	if len(sys.argv) < 2:
+		choice = random.choice(AVAILABLE_COMICS)
+	else:
+		choice = sys.argv[1] 
+
+	if choice == 'calvin': # default is calvin
+		# print ('inside')
 		exist = True
 		while exist:
-			year = random.choice(range(1985,1995))
-			month = random.choice(range(1,12))
-			day = random.choice(range(1,31))
+			year = random.choice(list(range(1985,1995)))
+			month = random.choice(list(range(1,12)))
+			day = random.choice(list(range(1,31)))
 
 			try:
 				datetime(year,month,day)
@@ -44,49 +53,58 @@ def getcomic():
 			#zfill to add 0 before a number for number less than 10
 
 			url = 'http://marcel-oehler.marcellosendos.ch/comics/ch/' + str(year) + '/' + str(month).zfill(2) + '/' + str(year) + str(month).zfill(2) + str(day).zfill(2) + '.gif'
-			print url
+			print (url)
 
 			try:
 				exist = False
-				res = urllib2.urlopen(url)
-				output = open(os.path.join(APP_IMAGES, 'calvin.gif'), 'wb')
+				image_path = os.path.join(APP_IMAGES, 'calvin.gif')
+				res = urllib.request.urlopen(url)
+				output = open(image_path, 'wb')
 				output.write(res.read())
 				output.close()
-				webbrowser.open(os.path.join(APP_IMAGES, 'calvin.gif'))
-			except Exception, error:
-				print error
+				if PLATFORM == 'darwin':
+					image_path = 'file://' + os.path.abspath(image_path)
+				webbrowser.open(image_path)
+			except Exception as error:
+				print (error)
 				pass
 			break
 
-	elif sys.argv[1] == 'xkcd':
+	elif choice == 'xkcd':
 		url = 'http://c.xkcd.com/random/comic/'
 		try:
-			page = urllib2.urlopen(url)
+			page = urllib.request.urlopen(url)
 			soup = BeautifulSoup(page, "html.parser")
 			img = 'http:' + soup.find('div',{'id':'comic'}).find('img')['src']
-			res = urllib2.urlopen(img)
-			output = open(os.path.join(APP_IMAGES, 'xkcd.png'), 'wb')
+			res = urllib.request.urlopen(img)
+			image_path = os.path.join(APP_IMAGES, 'xkcd.png')
+			output = open(image_path, 'wb')
 			output.write(res.read())
 			output.close()
-			webbrowser.open(os.path.join(APP_IMAGES, 'xkcd.png'))
-		except Exception, error:
-			print error
+			if PLATFORM == 'darwin':
+				image_path = 'file://' + os.path.abspath(image_path)
+			webbrowser.open(image_path)
+		except Exception as error:
+			print (error)
 			pass
 
-	elif sys.argv[1] == 'explosm':
+	elif choice == 'explosm':
 		url = 'http://explosm.net/comics/random/'
 		try:
-			page = urllib2.urlopen(url)
+			page = urllib.request.urlopen(url)
 			soup = BeautifulSoup(page, "html.parser")
 			img = 'http:' + soup.find('img',{'id':'main-comic'})['src']
-			print img
-			res = urllib2.urlopen(img)
-			output = open(os.path.join(APP_IMAGES, 'explosm.png'), 'wb')
+			print (img)
+			res = urllib.request.urlopen(img)
+			image_path = os.path.join(APP_IMAGES, 'explosm.png')
+			output = open(image_path, 'wb')
 			output.write(res.read())
 			output.close()
-			webbrowser.open(os.path.join(APP_IMAGES, 'explosm.png'))
-		except Exception, error:
-			print error
+			if PLATFORM == 'darwin':
+				image_path = 'file://' + os.path.abspath(image_path)
+			webbrowser.open(image_path)
+		except Exception as error:
+			print (error)
 			pass
 
 while True:
